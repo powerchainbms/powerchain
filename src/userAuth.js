1034a1acaaab3ad1f7a68d4abd2a3a1a090209ca
Mahesh-1997 <mahesh21319@gmail.com>
2019-03-31 23:36:09 +0530

    channel management implemented

--- src/userAuth.js
+++ src/userAuth.js
@@ -44,12 +44,14 @@ const login = async db => {
       var repass = readlineSync.question("Confirm New Password> ", {
         hideEchoBack: true
       });
+      var cName = await manageChannel(db);
       if (pass === repass) {
         //save into db as new user
         var userDetails = {
           userId: uid,
           password: pass,
           doj: new Date(),
+          channelName: cName,
           publicKey: wallet_1.getPublicFromWallet(),
           accountBalance: 0
         };
@@ -66,4 +68,46 @@ const login = async db => {
       return await login(db);
   }
 };
+
+manageChannel = async db => {
+  let choiceM = readlineSync.question(
+    "enter\n 1. TO start a new channel\n 2. To join a existing channel\n\n Answer>"
+  );
+  switch (parseInt(choiceM)) {
+    case 1:
+      let name1 = readlineSync.question("Enter your new Channel name: ");
+      let query1 = { name: name1 };
+      let result1 = await db.collection("Channels").findOne(query1);
+      if (result1) {
+        console.log(
+          "\n\nSorry another channel already exists with name: " +
+            name1 +
+            " ,Please retry\n"
+        );
+        return await manageChannel(db);
+      } else {
+        console.log("\nYour custom channel (" + name1 + ") is good to go\n");
+        await db.collection("Channels").insertOne(query1);
+        console.log("\nYour custom channel (" + name1 + ") has been created\n");
+        return name1;
+      }
+      break;
+    case 2:
+      let name2 = readlineSync.question(
+        "Enter name of the channel you want to join: "
+      );
+      let query2 = { name: name2 };
+      let result2 = await db.collection("Channels").findOne(query2);
+      if (result2) {
+        console.log("\n" + name2 + " channel exists, adding you into it...");
+        return name2;
+      } else {
+        console.log(
+          "\nThe channel (" + name2 + ") doesnt exists, Please retry.\n"
+        );
+        return await manageChannel(db);
+      }
+  }
+};
+
 exports.login = login;
